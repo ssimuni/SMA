8f2bbc979bc832361d6d33dcb832ccb241bdd344
unknown <ssimuni>
2024-04-01 23:06:25 +0600

    entry button perfect. but location is changing when i change place

--- app/src/main/java/com/example/simu/Attendance.java
+++ app/src/main/java/com/example/simu/Attendance.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
@@ -75,24 +76,20 @@ public class Attendance extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    // Fetch current time from NTP server
                     NTPUDPClient client = new NTPUDPClient();
                     InetAddress inetAddress = InetAddress.getByName("pool.ntp.org");
                     TimeInfo timeInfo = client.getTime(inetAddress);
                     long currentTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
 
-                    // Convert current time to Calendar instance
                     Calendar calendar = Calendar.getInstance();
                     calendar.setTimeInMillis(currentTime);
 
-                    // Check if current time is within the allowed time range (8:45 am to 9:15 am)
                     int hour = calendar.get(Calendar.HOUR_OF_DAY);
                     int minute = calendar.get(Calendar.MINUTE);
                     if ((hour == 8 && minute >= 45) || (hour == 9 && minute <= 15)) {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                // Enable the intime button
                                 intime.setEnabled(true);
                                 startActivity(new Intent(Attendance.this, Upload_attendance.class));
                             }
@@ -101,8 +98,8 @@ public class Attendance extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                // Disable the intime button
                                 intime.setEnabled(false);
+                                Toast.makeText(Attendance.this, "You are late!!!", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
